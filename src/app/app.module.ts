89541d15b1964be765a8bb03a652be91e0af9721
Christopher Enoch <christopher.enoch@yahoo.co.uk>
2022-06-05 13:08:27 +0200

    fetchPresWords method added to pres-words.service to get words from firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar/navbar.component';
 import { PresComponent } from './presentation/pres/pres.component';
 import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pres-words.service.ts
+++ src/app/pres-words.service.ts
@@ -1,16 +1,59 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Subject} from 'rxjs';
 import { PRESWORDS } from './mock-pres-words';
 import { PresWord } from './pres-word';
+import { HttpClient } from '@angular/common/http';
+import { map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PresWordsService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  private presWords: PresWord[];
 
   getPresWords(): Observable<PresWord[]> {
     const presWords = of(PRESWORDS);
     return presWords;
   }
+
+  fetchPresWords(){
+
+    return this.http.get<{ [key: string]: PresWord}>("https://flashcard-app-2022-default-rtdb.firebaseio.com/posts.json")
+    .pipe(
+      map( responseData =>
+        {
+          let wordsFetched:PresWord[] = [];
+          for (const key in responseData){
+            if (responseData.hasOwnProperty(key)){
+              console.log(" responseData[key] " + responseData[key]);
+              wordsFetched.push({...responseData[key]});
+            }
+          }
+          return wordsFetched;
+        })
+        , tap(
+          words => {
+            // console.log("Words from database " + JSON.stringify(words));
+            // console.log("Words from database " + JSON.stringify(Object.values(words)));
+            // console.log("Words from database " + JSON.stringify(Object.values(words[0])));
+            // console.log("Words from database " + JSON.stringify(words[0]));
+            this.presWords = Object.values(words[0]);
+            //console.log("Words 2" + this.words2);
+          })
+    )}
+
+
+
+
+
+
+
+
+
+
+
+
 }
