b9c329d83bb70e0bc306d335e434bc19f06a75fe
chrisenoch <christopher.enoch@yahoo.co.uk>
2022-09-10 17:17:00 +0200

    app navbar now only shows if user is logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy{
   title = 'presentation-app';
-  isLoggedIn = false; //changethis - move to other login class?
+  private isAuthSub: Subscription; 
+  isAuth:boolean;
+  
+  constructor(private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isAuthSub = this.authService.isAuthSub.subscribe(
+      data => {
+        this.isAuth = !!data;
+        console.log("value of data: " + data); });
+  }
+
+  ngOnDestroy(): void {
+    this.isAuthSub.unsubscribe();//
+  }
 
 }
 
